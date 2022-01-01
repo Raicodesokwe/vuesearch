0c14d8caf01ae73a12aabe5e90a546e8dd2bbde3
Raicodesokwe <muraierick254@gmail.com>
2022-01-01 12:28:26 +0300

    first commit

---
+++ src/dummy-data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 'obj',
+    fullName: 'objimilian Dare',
+    projects: [
+      { id: 'obj_p1', title: 'Record the Vue course' },
+      { id: 'obj_p2', title: 'Create more courses' },
+      { id: 'obj_p3', title: 'Keep content updated' }
+    ]
+  },
+  {
+    id: 'sujo',
+    fullName: 'sujoel Kolombo',
+    projects: [
+      { id: 'sujo_p1', title: 'Create more courses' },
+      { id: 'sujo_p2', title: 'Dive into data science topics' },
+      { id: 'sujo_p3', title: 'Various things' }
+    ]
+  },
+  {
+    id: 'sami',
+    fullName: 'sami Knowles',
+    projects: [
+      { id: 'sami_p1', title: 'Create agenda for next months' },
+      { id: 'sami_p2', title: 'Explore new topics' }
+    ]
+  },
+  {
+    id: 'fidra',
+    fullName: 'fidra Miller',
+    projects: []
+  }
+];
